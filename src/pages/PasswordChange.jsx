a04803fe0761c02f2e02ace107069f9507fce3b1
mr-atuzie <user email>
2024-03-02 15:25:19 +0100

    password change ui

--- src/pages/PasswordChange.jsx
+++ src/pages/PasswordChange.jsx
@@ -10,16 +10,16 @@ const PasswordChange = () => {
         <div className=" w-[60px] h-[60px] flex justify-center items-center bg-[#FFEFEA] rounded-full">
           <IoLockClosedOutline size={20} className=" text-[#FF5D2E]" />
         </div>
-        <h1 className=" text-[24px] font-medium text-center">
+        <h1 className=" text-[20px] lg:text-[24px] font-medium text-center">
           Password changed successfully!
         </h1>
-        <p className=" w-[340px] text-base text-[#8C8C8C] text-center">
+        <p className="text-xs lg:w-[340px] lg:text-base text-[#8C8C8C] text-center">
           Your password has been successfully updated
         </p>
 
         <button
           onClick={() => navigate("/admin-login")}
-          className=" rounded-lg text-center mt-6 text-white bg-[#FF5D2E] w-[400px] h-[55px]"
+          className=" rounded-lg text-center mt-6 text-white bg-[#FF5D2E] w-full mx-3 lg:mx-0 lg:w-[400px] h-[55px]"
         >
           Login
         </button>
